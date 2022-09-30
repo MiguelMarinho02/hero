8e6fb010143373b21116a8b452bc2d58a19547bf
Miguel <up202108822@up.pt>
2022-09-30 10:41:14 +0100

    Solved some error

--- src/main/java/com/MiguelMarinho02/hero/Main.java
+++ src/main/java/com/MiguelMarinho02/hero/Main.java
@@ -1,7 +1,6 @@
 package com.MiguelMarinho02.hero;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("HelloWorld!");
     }
 }
